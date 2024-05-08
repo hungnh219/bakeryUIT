37373c84d2f2c0b35cb2faac7dd35c6137258be0
thanhan2k <to.thanhan2k@gmail.com>
2024-05-08 22:33:18 +0700

    style: sửa liên hệ 2nd

--- client/src/pages/public/Contact.js
+++ client/src/pages/public/Contact.js
@@ -25,22 +25,22 @@ const Contact = () => {
         </div>
         <div className="w-full flex gap-x-4 justify-center my-6">
           <div className="flex-1 grid grid-cols-2 gap-2">
-            <div className="shadow rounded w-60 h-60 p-4">
+            <div className="hover:scale-105 hover:shadow-lg duration-300 shadow rounded w-60 h-60 p-4">
               <MdLocationOn color="#578899" size={40} />
               <h3 className="text-xl font-semibold">Địa chỉ</h3>
               <p>QL1A/1B 20, Khu Phố 6, Thủ Đức, Thành phố Hồ Chí Minh</p>
             </div>
-            <div className="shadow rounded w-60 h-60 p-4">
+            <div className="hover:scale-105 hover:shadow-lg duration-300 shadow rounded w-60 h-60 p-4">
               <FaClock color="#578899" size={40} />
               <h3 className="text-xl font-semibold">Giờ mở cửa</h3>
               <p>7h30SA - 11h30CH</p>
             </div>
-            <div className="shadow rounded w-60 h-60 p-4">
+            <div className="hover:scale-105 hover:shadow-lg duration-300 shadow rounded w-60 h-60 p-4">
               <RiPhoneFill color="#578899" size={40} />
               <h3 className="text-xl font-semibold">Điện thoại</h3>
               <p>0706770436</p>
             </div>
-            <div className="shadow rounded w-60 h-60 p-4">
+            <div className="hover:scale-105 hover:shadow-lg duration-300 shadow rounded w-60 h-60 p-4">
               <MdEmail color="#578899" size={40} />
               <h3 className="text-xl font-semibold">Email</h3>
               <p>uitcake@gmail.com</p>
